20c895f427f5e2b9a85045316f2267f06b74a8a4
Jorge Jesus Mendoza Arraiz <jorgemendoza.arraiz@gmail.com>
2024-07-30 09:33:22 -0400

    Layout Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,19 +20,30 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col text-center items-center  justify-center mt-4 p-0">
-        <Image
+        {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
 
-        <h1 className="mt-8">
+        {/* <h1 className="mt-8">
           <b>ESTAMPADOS</b> <br />
           PERSONALIZADOS
-        </h1>
+        </h1> */}
+        <div className="flex justify-center">
+          <Image
+            src="/images/H_LOGO.png"
+            width={0}
+            height={0}
+            className="logo"
+            alt=""
+            sizes="100vw"
+            // style={{ width: "20%", height: "auto" }} // optional
+          />
+        </div>
 
         <div className="flex flex-col gap-4 w-full justify-center items-center mt-4 social-column">
           {mockData.map((data) => (
@@ -41,12 +52,12 @@ export default function Home() {
         </div>
 
         <div className="sara-container w-full flex justify-end m-0">
-          <div className="bg-white flex justify-end sara-background">
+          <div className=" flex pl-5 justify-end sara-background">
             <Image
-              src="/images/woman_placeholder.png"
+              src="/images/V-SARA.png"
               width={0}
               height={0}
-              className="mt-[15%] sara-image"
+              className="mt-[15%] ml-[10%] sara-image"
               alt=""
               sizes="100vw"
               // style={{ width: "70%", height: "auto" }} // optional
