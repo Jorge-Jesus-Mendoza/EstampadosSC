e205144451c23937fa537cc2704340b181de6b72
Jorge Jesus Mendoza Arraiz <jorgemendoza.arraiz@gmail.com>
2024-08-14 17:21:46 -0400

    fixed translation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
             className="p-5 rounded-3xl download-button"
             target="_blank"
           >
-            Descarga nuestro catalogo
+            Mira nuestro catálogos
           </Link>
         )}
       </div>
